376cd8a846c5b516f154e64f59d38d7ee85758da
Sam Johnson <sambjohnson94@gmail.com>
2023-11-18 22:54:34 -0800

    add step0_repl

---
+++ impls/python/step0_repl.py
@@ -0,0 +1,34 @@
+import sys
+
+
+def READ(x):
+    return x
+
+
+def EVAL(x):
+    return x
+
+
+def PRINT(x):
+    return x
+
+
+def rep(x):
+    res_read = READ(x)
+    res_eval = EVAL(res_read)
+    res_print = PRINT(res_eval)
+    return res_print
+
+
+def main():
+    while True:
+        try:
+            user_input = input("user> ")
+            res_rep = rep(user_input)
+            print(res_rep)
+        except EOFError:
+            sys.exit("bye! :3")
+
+
+if __name__ == "__main__":
+    main()
